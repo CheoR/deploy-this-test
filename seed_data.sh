1b323a9299fdd87950dbd8c92f07267056c0a1bb
CheoR <cheo.roman+github@gmail.com>
2021-06-15 22:51:50 -0500

    build: automatically clean data tables, run migrations, migration, and reload data with fixtures.

--- seed_data.sh
+++ seed_data.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
 
-# Update this file for your project
+# Script clears app tables from the postgreql db.
+# Then recreates the migratoins and migrates to db.
+# Then loads your data into the tables.
 
-# rm -rf deploy_test/migrations
-# rm db.sqlite3
-python manage.py makemigrations deploy_test
-# python manage.py migrate
-# python manage.py loaddata users
-# python manage.py loaddata tokens
-# python manage.py loaddata customers
-# python manage.py loaddata product_category
-# python manage.py loaddata product
-# python manage.py loaddata productrating
-# python manage.py loaddata payment
-# python manage.py loaddata order
-# python manage.py loaddata order_product
-# python manage.py loaddata favoritesellers
+echo '\n==============='
+echo 'Dropping tables'
+echo '==============='
+python manage.py migrate app1 zero
+python manage.py migrate authtoken zero
+python manage.py migrate auth zero
+
+echo '\n========================'
+echo 'Deleteing app migrations'
+echo '========================'
+rm -rf app1/migrations
+
+
+echo '\n============================='
+echo 'Running makemigrations on app'
+echo '============================='
+python3 manage.py makemigrations app1
+
+echo '\n==============='
+echo 'Running migrate'
+echo '==============='
+python3 manage.py migrate
+
+# # python manage.py loaddata users
+# # python manage.py loaddata tokens
+# # python manage.py loaddata customers
+# # python manage.py loaddata product_category
+# # python manage.py loaddata product
+# # python manage.py loaddata productrating
+# # python manage.py loaddata payment
+# # python manage.py loaddata order
+# # python manage.py loaddata order_product
+# # python manage.py loaddata favoritesellers
+
+echo '\n================'
+echo 'Loading fixtures'
+echo '================'
+
+python manage.py loaddata data/fixtures/users
+python manage.py loaddata data/fixtures/tokens
+python manage.py loaddata data/fixtures/appusers
+
+echo '\n================'
+echo 'DONE'
+echo '================'
